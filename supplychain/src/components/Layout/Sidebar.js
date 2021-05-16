b5b0dfe854f3db441dc7ce0062389fe7d1329028
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2021-05-17 00:54:41 +0530

    menu name

--- supplychain/src/components/Layout/Sidebar.js
+++ supplychain/src/components/Layout/Sidebar.js
@@ -2,6 +2,7 @@ import logo200Image from 'assets/img/logo/logo_200.png';
 import sidebarBgImage from 'assets/img/sidebar/sidebar-4.jpg';
 import SourceLink from 'components/SourceLink';
 import React from 'react';
+import Typography from 'components/Typography';
 import { FaGithub } from 'react-icons/fa';
 import {
   MdAccountCircle,
@@ -110,18 +111,12 @@ class Sidebar extends React.Component {
         <div className={bem.e('background')} style={sidebarBackground} />
         <div className={bem.e('content')}>
           <Navbar>
-            <SourceLink className="navbar-brand d-flex">
-              <img
-                src={logo200Image}
-                width="40"
-                height="30"
-                className="pr-2"
-                alt=""
-              />
-              <span className="text-white">
-                SupplyChain
-              </span>
-            </SourceLink>
+            <Typography type="h3"><span className="text-white">
+            SupplyChain
+          </span></Typography>
+              
+              
+            
           </Navbar>
           <Nav vertical>
             {navItems.map(({ to, name, exact, Icon }, index) => (
