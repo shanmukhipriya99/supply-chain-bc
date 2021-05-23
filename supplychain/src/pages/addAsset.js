6a132e7d486c29578e8761144ead3c4fbb05f477
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2021-05-23 19:55:16 +0530

    add asset web3

--- supplychain/src/pages/addAsset.js
+++ supplychain/src/pages/addAsset.js
@@ -1,104 +1,141 @@
 import Page from 'components/Page';
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import {
   Button,
   Card,
+  CardTitle,
   CardBody,
   CardHeader,
   Col,
   Form,
-  FormFeedback,
   FormGroup,
-  FormText,
   Input,
   Label,
   Row,
 } from 'reactstrap';
 import axios from '../axios';
+import web3 from '../web3';
+import contract from '../contract';
 
 class AddAsset extends Component {
-
   state = {
-    aid: ''
-  }
+    aid: '',
+    message: null,
+    errorMessage: '',
+  };
 
   componentDidMount() {
-    axios.get("/getAID").then(response => {
+    // console.log(web3.version);
+    // web3.eth.getAccounts().then(console.log);
+    axios.get('/getAID').then(response => {
       console.log(response.data.id);
-      this.setState({ aid: response.data.id});
-    })
+      this.setState({ aid: response.data.id });
+    });
   }
-    
 
-    submitHandler = () => {
-        let asset = {
-            AID: this.state.aid,
-            AName: document.getElementById("AName").value,
-            creator: localStorage.getItem("PID"),
-            owner: localStorage.getItem("PID"),
-        }
-        axios.post("/createAsset", asset).then(response => {
-            // console.log(response);
-            if(response.status === 201){
-                alert("Asset Created!");
-                this.props.history.push("/dashboard");
-            }
-        }).catch(err => {
+  submitHandler = async () => {
+    let asset = {
+      AID: this.state.aid,
+      AName: document.getElementById('AName').value,
+      creator: localStorage.getItem('PID'),
+      owner: localStorage.getItem('PID'),
+    };
+    this.setState({ message: 'Creating Asset...' });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await contract.methods
+        .createAsset(asset.AID, asset.AName, asset.creator, asset.owner)
+        .send({
+          from: accounts[0],
+        });
+      axios
+        .post('/createAsset', asset)
+        .then(response => {
+          // console.log(response);
+          if (response.status === 201) {
+            this.setState({ message: null });
+            alert('Asset Created!');
+            this.props.history.push('/dashboard');
+          }
+        })
+        .catch(err => {
           if (err.response.status === 500) {
-            alert("Server error, please try again later!");
+            alert('Server error, please try again later!');
           } else if (err.response.status === 401) {
-            alert("Unauthorized!");
-            this.props.history.push("/");
+            alert('Unauthorized!');
+            this.props.history.push('/');
           }
         });
+    } catch (err) {
+      this.setState({ message: null });
+      this.setState({ errorMessage: err.message });
+      setTimeout( () => document.location.reload(), 4000);
+      // console.log(err.message);
     }
+  };
 
-    render() {
-        return (
-            <Page title="Add Asset" breadcrumbs={[{ name: 'Add Asset', active: true }]}>
-              <Row>
-                <Col xl={12} lg={12} md={12}>
-                  <Card>
-                    <CardHeader>Fill the details:</CardHeader>
-                    <CardBody>
-                      <Form>
-                        <FormGroup>
-                          <Label for="exampleEmail">Name</Label>
-                          <Input
-                            type="text"
-                            id="AName"
-                            placeholder="Asset Name"
-                          />
-                        </FormGroup>
-                        <FormGroup>
-                        <Label for="creator">Creator</Label>
-                        <Input
-                          plaintext
-                          value={localStorage.getItem("email")}
-                          readOnly
-                        />
-                      </FormGroup>
-                      <FormGroup>
-                        <Label for="owner">Owner</Label>
-                        <Input
-                          plaintext
-                          value={localStorage.getItem("email")}
-                          readOnly
-                        />
-                      </FormGroup>
-                      </Form>
-                      <Button color="secondary" onClick={() => this.submitHandler()}>
-                      Add
-                    </Button>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            </Page>
-          );
-        }
+  render() {
+    let card = null;
+    if (this.state.errorMessage !== '') {
+      card = (
+        <Card
+          style={{
+            marginTop: '20px',
+            padding: '10px'
+          }}
+          color={'danger'}
+          border="light"
+        >
+          <CardTitle tag="h5">Oops!!</CardTitle>
+          <CardBody>{this.state.errorMessage}</CardBody>
+        </Card>
+      );
     }
-    
-  
-  export default AddAsset;
-  
+    return (
+      <Page
+        title="Add Asset"
+        breadcrumbs={[{ name: 'Add Asset', active: true }]}
+      >
+        <Row>
+          <Col xl={12} lg={12} md={12}>
+            <Card>
+              <CardHeader>Fill the details:</CardHeader>
+              <CardBody>
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleEmail">Name</Label>
+                    <Input type="text" id="AName" placeholder="Asset Name" />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="creator">Creator</Label>
+                    <Input
+                      plaintext
+                      value={localStorage.getItem('email')}
+                      readOnly
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="owner">Owner</Label>
+                    <Input
+                      plaintext
+                      value={localStorage.getItem('email')}
+                      readOnly
+                    />
+                  </FormGroup>
+                </Form>
+                <Button color="secondary" onClick={() => this.submitHandler()}>
+                  Add
+                </Button>
+                <br />
+                {this.state.message}
+                {card}
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Page>
+    );
+  }
+}
+
+export default AddAsset;
